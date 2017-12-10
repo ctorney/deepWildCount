a9bd248a08f28843fbe5e77e2daac2a0163a20b8
Colin Torney <colin.j.torney@gmail.com>
2017-12-10 09:05:23 +0000

    sequential allocation of zooniverse data to clusters by user id

--- preprocessing/clusterCounts.py
+++ preprocessing/clusterCounts.py
@@ -2,53 +2,71 @@ import numpy as np
 import pandas as pd
 import sklearn.cluster as skc
 import scipy.sparse as ss
+from math import hypot
+
+
+def allocate2Cluster(xmarks, ymarks, xcluster, ycluster):
+    nm = len(xmarks)
+    nc = len(xcluster)
+    returnVals = -1*np.ones(nm)
+    distances = np.full((nm,nc), np.inf)
+
+    # calculate distances between marks and clusters
+    for i in range(nm):
+        for j in range(nc):
+            distances[i][j] = hypot(xmarks[i]-xcluster[j],ymarks[i]-ycluster[j])
+    # any marks over 64 pixels cannot belong to that cluster
+    distances[distances>64]=np.inf
+    
+    # loop while there are still potential matchings
+    while np.any(np.isfinite(distances)): 
+        # find the closest cluster-mark pairing
+        [i,j] = np.argwhere(distances == np.min(distances))[0]
+        # match the two closest
+        returnVals[i]=j
+        # set that cluster to infinity for all other marks 
+        distances[:,j]=np.inf
+
+    # anything left over now has a new cluster created
+    next_c=nc
+    for i in range(nm):
+        if returnVals[i]<0:
+            returnVals[i]=next_c
+            next_c+=1
+
+    return returnVals
+
+
+
+ROOTDIR = '../'
+image_dir = ROOTDIR + '/data/2015/'
+inputfile = ROOTDIR  + '/data/2015-Z-ALL-COUNTERS.csv'
+count_data = pd.read_csv(inputfile)
+count_data.drop_duplicates(inplace=True)
+
+data = []
 
 
 # uses clustering to group identifications by multiple counters into one average location
+for image in count_data['SWC_image'].drop_duplicates():
+    df = count_data.loc[count_data['SWC_image'] == image][['user_name', 'xcoord', 'ycoord']].dropna(axis=0)
+    df['cluster']=-1
+    for user, marks in df.groupby('user_name'):
+        cluster_locations = df[df['cluster']>=0].groupby(['cluster']).mean()
+        allocated = allocate2Cluster( marks.xcoord.values,marks.ycoord.values, cluster_locations.xcoord.values, cluster_locations.ycoord.values)
+        j=0
+        for i,m in marks.iterrows():
+            df.cluster[i]=allocated[j]
+            j+=1
+    c_count = df['cluster'].max()+1
 
-data_file_csv = '../data/swc_zooniverse_data_22Nov17.csv'
-count_data = pd.read_csv(data_file_csv)
-data = []
+    for cluster in range(c_count):
+        if len(df[df.cluster==cluster]) > 5:
+            Xx = df[df.cluster==cluster].xcoord.mean()
+            Yy = df[df.cluster==cluster].ycoord.mean()
+            data.append([image,Xx,Yy])
 
-for tile in count_data['tile_id'].drop_duplicates():
-    df = count_data.loc[count_data['tile_id'] == tile].dropna().groupby('user_name')['mark_index'].nunique()
-    if len(df.index) > 0:
-        accepted_number_of_marks = max(df)
-    else:
-        accepted_number_of_marks = 0
-    print('Tile: ' + tile + ', accepted marks: ' + str(accepted_number_of_marks))
-    if accepted_number_of_marks >= 2:
-        X = count_data.loc[count_data['tile_id'] == tile][['user_name', 'xcoord', 'ycoord']].dropna(axis=0)
-        # Blank coo matrix to csr
-        sm = ss.coo_matrix((X.shape[0], X.shape[0]), np.float32).tolil()
-        # Insert 1 for connected pairs and diagonals
-        for i in range(X.shape[0]):
-            for j in range(X.shape[0]):# add entries based on username, distance
-                if i == j:
-                    sm[i,j] = 1.0
-                else:
-                    if X.iloc[i]['user_name'] == X.iloc[j]['user_name']:
-                        sm[i,j] = 0.0005
-                    else:
-                        if (abs(X.iloc[i]['xcoord'] - X.iloc[j]['xcoord']) >= 32) or (abs(X.iloc[i]['ycoord'] - X.iloc[j]['ycoord']) >= 32):
-                            sm[i,j] = 0.0005
-                        else:
-                            sm[i,j] = 1.0
-        sm = sm.tocoo()  # convert back to coo format
-        model = skc.AgglomerativeClustering(accepted_number_of_marks, connectivity=sm, linkage='ward')
-        y = model.fit_predict(X[['xcoord', 'ycoord']])
-        X['cluster'] = y
-        X = X.groupby('cluster').filter(lambda x: len(x) >= 5)
-        X = X.groupby('cluster').mean()
-        X['tile_id'] = tile
-        for index, row in X.iterrows():
-            data.append([row.tile_id,index,row.xcoord,row.ycoord])
-    elif accepted_number_of_marks == 1:
-        X = count_data.loc[count_data['tile_id'] == tile].dropna(axis = 0)
-        if len(X) > 5:
-            Xx = sum(X['xcoord']) / X.shape[0]
-            Xy = sum(X['ycoord']) / X.shape[0]
-            data.append([tile,0,Xx,Xy])
 
-Y = pd.DataFrame(data, columns=['tile_id','cluster','xcoord','ycoord'])
-Y.to_csv('../data/swc_zooniverse_cluster_found_coords.csv')
+
+Y = pd.DataFrame(data, columns=['image_name','xcoord','ycoord'])
+Y.to_csv('../data/2015-FINAL-FINAL.csv')
